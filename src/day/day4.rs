847d9b3cb3c894635c1b579b1c5cce9b2aa17253
Helio Chissini de Castro <heliocastro@gmail.com>
2024-12-04 23:05:33 +0100

    feat: Add day4
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- src/day/day4.rs
+++ src/day/day4.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Helio Chissini de Castro
 //
 // SPDX-License-Identifier: GPL-2.0
+
 use crate::core::data::read_input;
 use crate::core::pos::{Direction, FromUsize, Pos};
 use crate::core::settings::Settings;
@@ -41,36 +42,49 @@ fn match_word(matrix: &Vec<Vec<char>>, word: &str) -> i32 {
 
 fn match_xmas(matrix: &Vec<Vec<char>>) -> i32 {
     // Create vec with all lines
-    let mut amount: i32 = 0;
+    let first_letter = 'A';
+    let mut amount = 0;
 
     for row in 0..matrix.len() {
         for col in 0..matrix[0].len() {
-            if matrix[row][col] == 'M' {
-                let mut pos = Pos::from_usize(col, row);
-                let mut step_amount: i32 = 0;
-                println!("\nFound initial letter M at: {:?} {:?}", pos, matrix[row]);
-                walk_around(matrix, &pos, "AS", &mut step_amount, Direction::DownRight);
-                if pos.x + 2 < matrix[0].len() as isize {
-                    pos = Pos::from_usize(col + 2, row);
-                    walk_around(matrix, &pos, "AS", &mut step_amount, Direction::DownLeft);
-                }
-                if pos.y + 2 < matrix.len() as isize {
-                    pos = Pos::from_usize(col, row + 2);
-                    walk_around(matrix, &pos, "AS", &mut step_amount, Direction::UpRight);
-                }
-                if pos.y + 2 < matrix.len() as isize {
-                    pos = Pos::from_usize(col+2, row + 2);
-                    walk_around(matrix, &pos, "AS", &mut step_amount, Direction::UpLeft);
-                }
-                if step_amount == 2 {
-                    amount += 1;
-                }
-                println!("AMOUNT: {}", amount)
+            if matrix[row][col] == first_letter {
+                let pos = Pos::from_usize(col, row);
+                // println!("INITIAL POS: {:?}", pos);
+                surround_me(matrix, &pos, &mut amount);
             }
         }
     }
 
     amount
+}
+
+fn surround_me(matrix: &Vec<Vec<char>>, pos: &Pos, amount: &mut i32) -> bool {
+    let (width, height) = (matrix[0].len() as isize, matrix.len() as isize);
+    let surround: Vec<Pos> = vec![
+        pos.move_dir(Direction::UpLeft),
+        pos.move_dir(Direction::UpRight),
+        pos.move_dir(Direction::DownLeft),
+        pos.move_dir(Direction::DownRight),
+    ];
+
+    for letter in "MS".chars() {
+        let mut total = 0;
+        for s in &surround {
+            if s.x < 0 || s.y < 0 || s.x == width as isize || s.y == height as isize {
+                // println!("SKIP LETTER: {}", letter);
+                continue;
+            }
+            if matrix[s.y as usize][s.x as usize] == letter {
+                // println!("--- {:?} {}", s, letter);
+                total += 1;
+            }
+        }
+        if total != 2 {
+            return false;
+        }
+    }
+    *amount += 1;
+    return true;
 }
 
 fn walk_around(
@@ -79,7 +93,7 @@ fn walk_around(
     word: &str,
     amount: &mut i32,
     direction: Direction,
-) {
+) -> bool {
     let next_char = word.chars().nth(0);
 
     let npos = pos.move_dir(direction);
@@ -89,23 +103,21 @@ fn walk_around(
         || npos.y >= matrix.len() as isize
         || npos.x >= matrix[0].len() as isize
     {
-        return;
+        return false;
     }
     if let Some(next_char) = next_char {
         let remaining = &word[1..];
-        println!(
-            "NEXT: {} == {} at {:?}, Direction: {:?}",
-            matrix[npos.y as usize][npos.x as usize], next_char, npos, direction,
-        );
         if matrix[npos.y as usize][npos.x as usize] == next_char {
             walk_around(matrix, &npos, remaining, amount, direction);
         } else {
-            return;
+            return false;
         }
     }
     if word.len() == 1 {
         *amount += 1;
+        return true;
     }
+    false
 }
 
 fn create_matrix(input: &str) -> Vec<Vec<char>> {
